8771d0ee535ab07c04baf3efaf79a006ff00137a
stevebstorm <steve.lorent@gmail.Com>
2023-06-05 14:42:25 +0200

    Exo Quizz

---
+++ BlazorDemoWASM/Pages/Exo/Exo1/Questions.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemoWASM.Pages.Exo.Exo1
+{
+    public partial class Questions
+    {
+        [Parameter]
+        public string Username { get; set; }
+
+        [Parameter]
+        public EventCallback NotifyEndGame { get; set; }
+        [Parameter]
+        public EventCallback<string> NotifyAnswer { get; set; }
+
+        public List<string> QuestionList { get; set; }
+        public int Counter { get; set; }
+        public string CurrentQuestion { get; set; }
+
+        public Questions()
+        {
+            QuestionList = new List<string>();
+            Counter = 0;
+            QuestionList.Add("Vous avez bien manger ?");
+            QuestionList.Add("Pas trop mal à la tête ?");
+            QuestionList.Add("il fait trop chaud ?");
+
+            CurrentQuestion = QuestionList[Counter];
+            Counter++;
+        }
+
+        public void Answer(string resp)
+        {
+            NotifyAnswer.InvokeAsync(resp);
+            if(Counter >= QuestionList.Count)
+            {
+                CurrentQuestion = "";
+                NotifyEndGame.InvokeAsync();
+            }
+            else
+            {
+                CurrentQuestion = QuestionList[Counter];
+            }
+            Counter++;
+        }
+    }
+}
---
+++ BlazorDemoWASM/Pages/Exo/Exo1/Quizz.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorDemoWASM.Pages.Exo.Exo1
+{
+    public partial class Quizz
+    {
+        public string Username { get; set; }
+        public bool QuizzIsFinished { get; set; }
+
+        public List<string> Answers { get; set; }
+        public Quizz()
+        {
+            Answers = new List<string>();
+        }
+
+        public void ReceiveAnswer(string response)
+        {
+            Answers.Add(response);
+        }
+
+        public void NotifiedEndGame()
+        {
+            QuizzIsFinished = true;
+        }
+    }
+}
